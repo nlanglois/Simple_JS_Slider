25ee74a7fd7bc3187984be9cac5dcab3414984fe
Nikolai Langlois <nlangloi10@umphysicians.umn.edu>
2016-07-20 00:17:55 -0500

    Added image alt tag to be used as slide title Added setInterval to have slider advance every x seconds

--- slider.js
+++ slider.js
@@ -6,6 +6,7 @@ window.onload = function() {
 
     // Declare app variables
     var slider = document.getElementsByClassName('slider')[0],
+        title = document.getElementsByClassName('title')[0],
         images = slider.getElementsByTagName('img'),
         counter = 0,
         nextBtn = slider.getElementsByClassName('next')[0],
@@ -20,13 +21,16 @@ window.onload = function() {
         }
         // Add the showImage class name to the img-element you want
         images[index].className = 'showImage';
+
+        // Take the content of the alt tag and write it inside the title div element
+        title.innerHTML = images[index].alt;
     }
 
 
     // Gets next image, invoked on nextBtn press
     function nextImg() {
-        // counter variable gets the current img selected
-        // if we have the last img, we switch back to the first one again
+        // Counter variable gets the current img selected
+        // If we have the last img, we switch back to the first one again
         if (counter < images.length -1) {
             counter += 1;
         } else {
@@ -38,8 +42,8 @@ window.onload = function() {
 
     // Gets previous image, invoked on prevBtn press
     function prevImg() {
-        // counter variable gets the current img selected
-        // if we have the first img, we switch back to the last one again
+        // Counter variable gets the current img selected
+        // If we have the first img, we switch back to the last one again
         if (counter > 0) {
             counter -= 1;
         } else {
@@ -49,12 +53,16 @@ window.onload = function() {
     }
 
 
-    // give the buttons an onclick event
+    // Give the buttons an onclick event
     nextBtn.onclick = nextImg;
     prevBtn.onclick = prevImg;
 
 
-    // start the slider
+    // Says for the nextImg method to be called automatically every 10000 milliseconds (10 seconds)
+    window.setInterval(nextImg, 10000);
+
+
+    // Start the slider
     showImage(counter);
 
 };
